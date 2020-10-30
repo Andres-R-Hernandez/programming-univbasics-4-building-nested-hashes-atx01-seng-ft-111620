1b1845233f61d3ff774a89a62975fcc9e71fa615
Andres Hernandez <andres.hernandez@utexas.edu>
2020-10-30 22:15:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,9 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  money = base_hash
+  money = {
+    :railroads => {}
+  }
   money[:railroads] = {
     :pieces => 4,
     :rent_in_dollars => {},

d100fd6c45cbf66001f27affec320db1d03de1d2
Andres Hernandez <andres.hernandez@utexas.edu>
2020-10-30 22:11:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly_with_second_tier
+  money = monopoly_with_second_tier
   money[:railroads][:rent_in_dollars] = {
     :one_piece_owned => 25,
     :two_pieces_owned => 50,
